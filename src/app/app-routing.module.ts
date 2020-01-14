8bb549c5f83039e1c92ade1e50aa497df34b9232
mark_anuchit <32482980+senategit@users.noreply.github.com>
2020-01-14 14:45:34 +0700

    (fix) new file project chatonline

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule, Component } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  {
+    path: "login",
+    component : LoginComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
